396188e7e8accdee90c0e721fcb1ca5e6f08eddf
Dhinakaran Chandrasekar <158206345+DhinakaranChandrasekar@users.noreply.github.com>
2025-04-23 13:22:43 +0400

    push for navigation

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -5,7 +5,7 @@ const Resume: React.FC = () => {
   return (
     <div className="resume-container">
       <iframe
-        src="/DhinaKaran_Chandrasekar_CV.pdf#zoom=100"
+        src={`${process.env.PUBLIC_URL}/DhinaKaran_Resume.pdf#zoom=100`}
         title="DhinaKaran Resume"
         className="resume-frame"
       ></iframe>
